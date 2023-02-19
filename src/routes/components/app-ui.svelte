35a73492be43fb9dc682612b38cc13ece55f93d5
nkarl <21993921+nkarl@users.noreply.github.com>
2023-02-18 17:26:59 -0800

    Updated the sketch of the app UI.

--- src/routes/components/app-ui.svelte
+++ src/routes/components/app-ui.svelte
@@ -10,11 +10,27 @@
 <div class="app-container">
 	<!-- Welcome Message -->
 	<div class="panel">
-		<div class="card">Card 1</div>
+		<h2>State List DropDown</h2>
+		<select id="data-option-list" name="">
+			<option id="item01" value="">State 01</option>
+			<option id="item02" value="">State 02</option>
+			<option id="item03" value="">State 03</option>
+			<option id="item04" value="">State 04</option>
+			<option id="item05" value="">State 05</option>
+		</select>
+		<h2>City List</h2>
 		<div class="card">Card 2</div>
 	</div>
 	<div class="panel">
-		<div class="card">Card 3</div>
+		<h2>City List DropDown</h2>
+		<select id="data-option-list" name="">
+			<option id="item01" value="">City 01</option>
+			<option id="item02" value="">City 02</option>
+			<option id="item03" value="">City 03</option>
+			<option id="item04" value="">City 04</option>
+			<option id="item05" value="">City 05</option>
+		</select>
+		<h2>Business List</h2>
 		<div class="card">Card 4</div>
 	</div>
 </div>
@@ -31,11 +47,11 @@
 	}
 
 	.panel {
-		border: 5px solid white;
+		border: 5px solid gray;
 		width: 1000px;
 		height: 75rm;
 		margin: 5px 5px;
-		padding: 5px 5px;
+		padding: 2% 2%;
 		flex-direction: row wrap;
 		border-radius: 25px;
 	}
@@ -47,5 +63,14 @@
 		margin: 10px 10px;
 		padding: 10px 10px;
 		border-radius: 25px;
+	}
+
+	#data-option-list {
+		width: 90%;
+		margin-left: 2%;
+		margin-right: 2%;
+		margin-bottom: 5%;
+		height: 5%;
+		font-size: 1rm;
 	}
 </style>
